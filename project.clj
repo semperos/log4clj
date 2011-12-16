78017ac1d3b1e0c337f5d18897a32601cb5f0861
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-12-16 14:20:10 -0500

    Set group and artifact appropriately in project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject com.semperos.log4clj "0.1.0-SNAPSHOT"
+(defproject com.semperos/log4clj "0.1.0-SNAPSHOT"
   :description "Convenience API over parts of Log4j"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [log4j/log4j "1.2.16"]])
+                 [log4j/log4j "1.2.16"]]
+  :aot [com.semperos.log4clj.appender.swing-appender])
